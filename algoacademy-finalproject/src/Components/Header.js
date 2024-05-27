3ecd09c708f79cb6ecb6558e588a53349ca37eec
muhammadali00-dev <ehmedovmehemmedeli0@gmail.com>
2024-05-27 21:16:49 +0400

    joystick added and header words repaired

--- algoacademy-finalproject/src/Components/Header.js
+++ algoacademy-finalproject/src/Components/Header.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Design/Header.css";
-import Logo from "../Images/cs-goLogo.svg";
+import Logo from "../Images/LOGO.svg";
+import joystick from "../Images/joy_stick.png";
 
 function Header() {
   return (
-    <div>
-      <img src={Logo} className="cslg"></img>
-      <Link to="#">Home</Link>
-      <Link to="#">About us</Link>
-      <Link to="#">Portfolio</Link>
-      <Link to="#">News</Link>
-      <Link to="#">Contact us</Link>
+    <div className="maindiv">
+      <img src={Logo} className="headlogo"></img>
+
+      <div className="bothlink">
+        <div className="links">
+          <Link to="#">Home</Link>
+          <Link to="#">About us</Link>
+          <Link to="#">Portfolio</Link>
+          <Link to="#">News</Link>
+        </div>
+        <div className="contactlink">
+          <Link to="#" className="btn-contact">
+            Contact us
+          </Link>
+        </div>
+      </div>
+
+      <img src={joystick} className="joystick"></img>
     </div>
   );
 }
